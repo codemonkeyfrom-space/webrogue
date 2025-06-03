9b6bde1ce97ff75a4ea9dd0afc5fbf986a1eaf52
Paul Abrams <john.paul.abrams@gmail.com>
2025-06-03 16:45:32 -0400

    fix (devops): mapgenDemo.html doesn't go in public

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        mapgen: 'mapgenDemo.html',  // the name "mapgen" here is arbitrary
+      },
+    },
     outDir: 'dist', // where Vite puts the built files
     emptyOutDir: true, // optional, cleans dist before build
   },
